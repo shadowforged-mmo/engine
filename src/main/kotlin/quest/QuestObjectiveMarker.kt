b037ef62905750e13c1b0a65e035e348aae1e3bd
Connor Reinholdtsen <connorreinh@gmail.com>
2026-04-04 15:31:37 -0700

    Initial schemas

--- src/main/kotlin/quest/QuestObjectiveMarker.kt
+++ src/main/kotlin/quest/QuestObjectiveMarker.kt
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.shadowforgedmmo.engine.instance.InstanceReference
 import com.shadowforgedmmo.engine.math.Polygon
 import com.shadowforgedmmo.engine.math.Vector2
-import com.shadowforgedmmo.engine.music.SongReference
 import java.awt.Graphics
 
 @JsonTypeInfo(
@@ -22,15 +21,18 @@ abstract class QuestObjectiveMarker {
     abstract fun draw(graphics: Graphics)
 }
 
-class PointQuestObjectiveMarker(private val point: Vector2) : QuestObjectiveMarker() {
+class PointQuestObjectiveMarker(
+    @JsonProperty("instance") private val instanceReference: InstanceReference,
+    @JsonProperty("point") private val point: Vector2
+) : QuestObjectiveMarker() {
     override fun draw(graphics: Graphics) {
         graphics.fillOval(point.x.toInt(), point.y.toInt(), 10, 10)
     }
 }
 
 data class PolygonQuestObjectiveMarker(
-    @JsonProperty("boundary") private val boundary: Polygon,
-    @JsonProperty("instance") private val instanceReference: InstanceReference
+    @JsonProperty("instance") private val instanceReference: InstanceReference,
+    @JsonProperty("boundary") private val boundary: Polygon
 ) : QuestObjectiveMarker() {
     override fun draw(graphics: Graphics) {
         val xs = boundary.points.map { it.x.toInt() }.toIntArray()
