102ee1f1f8acd2211cc70bd8b5329f57735f5260
Connor Reinholdtsen <connorreinh@gmail.com>
2026-04-08 14:38:28 -0700

    Remove env vars from config

--- src/main/kotlin/resource/Config.kt
+++ src/main/kotlin/resource/Config.kt
@@ -3,7 +3,5 @@ package com.shadowforgedmmo.engine.resource
 import com.fasterxml.jackson.annotation.JsonProperty
 
 class Config(
-    @JsonProperty("name") val name: String,
-    @JsonProperty("resource_pack_uri") val resourcePackUri: String,
-    @JsonProperty("mojang_auth") val mojangAuth: Boolean
+    @JsonProperty("name") val name: String
 )
